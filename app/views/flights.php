50549573ac48494328f6a2e8fb94da66cfc08222
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-12-30 21:06:23 +0500

    Update flights.php

--- app/views/flights.php
+++ app/views/flights.php
@@ -1,4 +1,5 @@
 <iframe src="https://secl-pk-uniquetravel.azurewebsites.net/language/en-US/flight/agentid/uniquetravel/currency/PKR/triptype/OW/cabin/E/routing/20210121-LHE-DXB/bFreeBaggage/false?pax=ADT-3,CNN-2_2_3,INF-1" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:110px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe>
 <style>
 footer{display:none}
+body {background-color: #e6edf6;}
 </style>
